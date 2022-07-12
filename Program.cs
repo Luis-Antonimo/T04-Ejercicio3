26fb640ba27fae0e9de7a091969ed181c5d4048f
Luis Antónimo <luisantonimo@gmail.com>
2022-07-12 08:37:35 +0200

    ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace T04Ejercicio3
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Diga su nombre:");
+            String nombre = Console.ReadLine();
+            Console.WriteLine("Diga su edad:");
+            //Se necesita convertir el string que retorna ReadLine
+            //a una variable numérica
+            int edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hola " + nombre + " tienes " + edad + " años.");
+        }
+    }
+}
